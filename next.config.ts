af37aeb2644495396df11e4e45acf311c5ea166a
Lance Kent <lancemagollado46@gmail.com>
2026-08-16 17:37:27 +0800

    build(next): updated for network workin

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: ["192.168.100.10"],
   typescript: {
     ignoreBuildErrors: false,
   },
